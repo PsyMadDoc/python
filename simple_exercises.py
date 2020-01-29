9554dd419ba37763a20d5b0112bf65f66ff183d4
Victor Cruceanu <v.d.cruceanu@gmail.com>
2020-01-29 12:50:53 +0200

    adding more exercises

--- simple_exercises.py
+++ simple_exercises.py
@@ -282,8 +282,205 @@ def same_name(your_name, my_name):
 
 ####################
 
+# Create a function named double_index that has two parameters, a list named lst and a single number as index.
+# The function should return a new list where all elements are the same as in lst except for the element at index, which should be double the value of the element at index of lst.
+# If index is not a valid index, the function should return the original list.
+# For example, the following code should return [1,2,6,4] because the element at index 2 has been doubled:
+# double_index([1, 2, 3, 4], 2)
+
+# Write your function here
+def double_index(lst, index):
+    if index >= len(lst):
+        print('not a valid index')
+    else:
+        # double_index = lst[index]*2
+        new_lst = lst[:index]
+        new_lst.append(lst[index]*2)
+        new_lst += lst[index+1:]
+        return new_lst
+
+# Uncomment the line below to test your function
+# print(double_index([3, 8, -10, 12], 2))
 
 ####################
 
+# Create a function named remove_middle which has three parameters named lst, start, and end.
+# The function should return a list where all elements in lst with an index between start and end (inclusive) have been removed.
+# For example, the following code should return [4, 23, 42] because elements at indices 1, 2, and 3 have been removed:
+# remove_middle([4, 8 , 15, 16, 23, 42], 1, 3)
 
+# Write your function here
+
+
+def remove_middle(lst, start, end):
+    if start < 0:
+        print('start parameter invalid')
+    elif end > len(lst):
+        print('end parameter invalid')
+    else:
+        new_lst = lst[:start] + lst[end+1:]
+        return new_lst
+
+# Uncomment the line below when your function is done
+# print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
+
+####################
+
+# Create a function named more_than_n that has three parameters named lst, item, and n.
+# The function should return True if item appears in the list more than n times. The function should return False otherwise.
+
+# Write your function here
+
+
+def more_than_n(lst, item, n):
+    counter = 0
+    for i in lst:
+        if i == item:
+            counter += 1
+    if counter > n:
+        return True
+    else:
+        return False
+
+# Uncomment the line below when your function is done
+# print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
+
+####################
+
+# Write your function here
+
+
+def more_frequent_item(lst, item1, item2):
+    item1_counter = 0
+    item2_counter = 0
+    for i in lst:
+        if i == item1:
+            item1_counter += 1
+        elif i == item2:
+            item2_counter += 1
+        else:
+            return 'neither items appear in list'
+    if item1_counter >= item2_counter:
+        return item1
+    else:
+        return item2
+
+# Uncomment the line below when your function is done
+# print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))
+
+####################
+
+# Create a function called middle_element that has one parameter named lst.
+# If there are an odd number of elements in lst, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements.
+
+# Write your function here
+
+
+def middle_element(lst):
+    # get index om middle elem
+    index = len(lst) / 2
+    index = int(index)
+
+    # check if list has odd/even num of elems
+    if len(lst) % 2 == 0:
+        # return avg of middle elem
+        avg = (lst[index - 1] + lst[index]) / 2
+        return avg
+    else:
+        # return middle elem
+        return lst[index]
+
+
+# Uncomment the line below when your function is done
+print(middle_element([5, 2, -10, -4, 4, 5]))
+
+####################
+
+# Write a function named append_sum that has one parameter named lst.
+# The function should add the last two elements of lst together and append the result to lst. It should do this process three times and then return lst.
+# For example, if lst started as [1, 1, 2], the final result should be [1, 1, 2, 3, 5, 8].
+
+# Write your function here
+
+
+def append_sum(lst):
+    i = 3
+    while i > 0:
+        lst.append(lst[-1] + lst[-2])
+        i -= 1
+    return lst
+
+# Uncomment the line below when your function is done
+# print(append_sum([1, 1, 2]))
+
+####################
+
+# Write a function named larger_list that has two parameters named lst1 and lst2.
+# The function should return the last element of the list that contains more elements. If both lists are the same size, then return the last element of lst1.
+
+# Write your function here
+
+
+def larger_list(lst1, lst2):
+    if len(lst2) > len(lst1):
+        return lst2[-1]
+    else:
+        return lst1[-1]
+
+# Uncomment the line below when your function is done
+# print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+####################
+
+# Write a function named combine_sort that has two parameters named lst1 and lst2.
+# The function should combine these two lists into one new list and sort the result. Return the new sorted list.
+
+# Write your function here
+
+
+def combine_sort(lst1, lst2):
+    new_lst = lst1 + lst2
+    return sorted(new_lst)
+
+# Uncomment the line below when your function is done
+# print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+####################
+
+# Create a function called append_size that has one parameter named lst.
+# The function should append the size of lst (inclusive) to the end of lst. The function should then return this new list.
+
+# Write your function here
+
+
+def append_size(lst):
+    lst.append(len(lst))
+    return lst
+
+# Uncomment the line below when your function is done
+# print(append_size([23, 42, 108]))
+
+####################
+
+# Create a function called every_three_nums that has one parameter named start.
+# The function should return a list of every third number between start and 100 (inclusive). For example, every_three_nums(91) should return the list [91, 94, 97, 100]. If start is greater than 100, the function should return an empty list.
+
+# Write your function here
+
+
+def every_three_nums(start):
+    return list(range(start, 101, 3))
+
+# Uncomment the line below when your function is done
+# print(every_three_nums(91))
+
+####################
+####################
+####################
+####################
+####################
+####################
+####################
+####################
+####################
 ####################
