adfe9ac38f9eb11d7bf6c1054d0c32057dbc1852
Victor Cruceanu <v.d.cruceanu@gmail.com>
2020-02-03 18:33:13 +0200

    adding exercises

--- simple_exercises.py
+++ simple_exercises.py
@@ -664,13 +664,89 @@ def reversed_list(lst1, lst2):
     return True
 
 # Uncomment the lines below when your function is done
-#print(reversed_list([1, 2, 3], [3, 2, 1]))
-#print(reversed_list([1, 5, 3], [3, 2, 1]))
+# print(reversed_list([1, 2, 3], [3, 2, 1]))
+# print(reversed_list([1, 5, 3], [3, 2, 1]))
 
 ####################
+
+# Write a function called unique_english_letters that takes the string word as a parameter. The function should return the total number of unique letters in the string. Uppercase and lowercase letters should be counted as different letters.
+
+
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+# Write your unique_english_letters function here:
+
+
+def unique_english_letters(word):
+    count = 0
+    for letter in letters:
+        if letter in word:
+            count += 1
+    return count
+# Uncomment these function calls to test your function:
+# print(unique_english_letters("mississippi"))
+# should print 4
+# print(unique_english_letters("Appleaa"))
+# should print 4
+
 ####################
+
+# Write a function named count_char_x that takes a string named word and a single character named x as parameters. The function should return the number of times x appears in word.
+
+# Write your count_char_x function here:
+
+
+def count_char_x(word, x):
+    count = 0
+    for i in range(len(word)):
+        if x in word[i]:
+            count += 1
+    return count
+
+# Uncomment these function calls to test your tip function:
+# print(count_char_x("mississippi", "s"))
+# should print 4
+# print(count_char_x("mississippi", "m"))
+# should print 1
+
 ####################
+
+# Write a function named count_multi_char_x that takes a string named word and a string named x. This function should do the same thing as the count_char_x function you just wrote - it should return the number of times x appears in word. However, this time, make sure your function works when x is multiple characters long.
+
+# Write your count_multi_char_x function here:
+
+
+def count_multi_char_x(word, x):
+    splits = word.split(x)
+    print(splits)
+    return(len(splits)-1)
+
+# Uncomment these function calls to test your  function:
+# print(count_multi_char_x("mississippi", "iss"))
+# should print 2
+# print(count_multi_char_x("apple", "pp"))
+# should print 1
+
 ####################
+
+# Write a function named substring_between_letters that takes a string named word, a single character named start, and another character named end. This function should return the substring between the first occurrence of start and end in word. If start or end are not in word, the function should return word.
+
+# Write your substring_between_letters function here:
+
+
+def substring_between_letters(word, start, end):
+    if start and end in word:
+        result = word[word.index(start)+1:word.index(end)]
+    else:
+        result = word
+    return result
+
+
+# Uncomment these function calls to test your function:
+# print(substring_between_letters("apple", "p", "e"))
+# should print "pl"
+# print(substring_between_letters("apple", "p", "c"))
+# should print "apple"
+
 ####################
 ####################
 ####################
