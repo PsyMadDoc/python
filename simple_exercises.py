6d55075ea98744c7f35dae3116f8049ef13834ae
Victor Cruceanu <v.d.cruceanu@gmail.com>
2020-02-04 16:32:29 +0200

    adding more exercises

--- simple_exercises.py
+++ simple_exercises.py
@@ -748,8 +748,119 @@ def substring_between_letters(word, start, end):
 # should print "apple"
 
 ####################
+
+# Create a function called x_length_words that takes a string named sentence and an integer named x as parameters. This function should return True if every word in sentence has a length greater than or equal to x.
+
+# Write your x_length_words function here:
+def x_length_words(sentance, x):
+    sentance_lst = sentance.split()
+    for i in range(len(sentance_lst)):
+        if len(sentance_lst[i]) < x:
+            return False
+        else:
+            return True
+
+
+# Uncomment these function calls to test your tip function:
+# print(x_length_words("i like apples", 2))
+# should print False
+# print(x_length_words("he likes apples", 2))
+# should print True
+
 ####################
+
+# Write a function called check_for_name that takes two strings as parameters named sentence and name. The function should return True if name appears in sentence in all lowercase letters, all uppercase letters, or with any mix of uppercase and lowercase letters. The function should return False otherwise.
+
+# Write your check_for_name function here:
+def check_for_name(sentance, name):
+    if name.lower() in sentance.lower():
+        return True
+    else:
+        return False
+
+# Uncomment these function calls to test your  function:
+# print(check_for_name("My name is Jamie", "Jamie"))
+# should print True
+# print(check_for_name("My name is jamie", "Jamie"))
+# should print True
+# print(check_for_name("My name is Samantha", "Jamie"))
+# should print False
+
 ####################
+
+# Create a function named every_other_letter that takes a string named word as a parameter. The function should return a string containing every other letter in word.
+
+# Write your every_other_letter function here:
+
+
+def every_other_letter(word):
+    other_letters = ''
+    if len(word) > 0:
+        for i in range(0, len(word), 2):
+            other_letters += word[i]
+    return other_letters
+# Uncomment these function calls to test your function:
+# print(every_other_letter("Codecademy"))
+# should print Cdcdm
+# print(every_other_letter("Hello world!"))
+# should print Hlowrd
+# print(every_other_letter(""))
+# should print
+
 ####################
+
+# Write a function named reverse_string that has a string named word as a parameter. The function should return word in reverse.
+
+# Write your reverse_string function here:
+
+
+def reverse_string(word):
+    return word[::-1]
+
+# Uncomment these function calls to test your  function:
+# print(reverse_string("Codecademy"))
+# should print ymedacedoC
+# print(reverse_string("Hello world!"))
+# should print !dlrow olleH
+# print(reverse_string(""))
+# should print
+
 ####################
+
+# A Spoonerism is an error in speech when the first syllables of two words are switched. For example, a Spoonerism is made when someone says “Belly Jeans” instead of “Jelly Beans”.
+# Write a function called make_spoonerism that takes two strings as parameters named word1 and word2. Finding the first syllable of a word is a difficult task, so for our function, we’re going to switch the first letters of each word. Return the two new words as a single string separated by a space.
+
+# Write your make_spoonerism function here:
+
+
+def make_spoonerism(word1, word2):
+    new_word1 = word2[0] + word1[1:]
+    new_word2 = word1[0] + word2[1:]
+    return new_word1 + ' ' + new_word2
+
+# Uncomment these function calls to test your function:
+# print(make_spoonerism("Codecademy", "Learn"))
+# should print Lodecademy Cearn
+# print(make_spoonerism("Hello", "world!"))
+# should print wello Horld!
+# print(make_spoonerism("a", "b"))
+# should print b a
+
+####################
+
+# Create a function named add_exclamation that has one parameter named word. This function should add exclamation points to the end of word until word is 20 characters long. If word is already at least 20 characters long, just return word.
+
+# Write your add_exclamation function here:
+
+
+def add_exclamation(word):
+    for i in range(20 - len(word)):
+        word += '!'
+    return word
+# Uncomment these function calls to test your function:
+# print(add_exclamation("Codecademy"))
+# should print Codecademy!!!!!!!!!!
+# print(add_exclamation("Codecademy is the best place to learn"))
+# should print Codecademy is the best place to learn
+
 ####################
